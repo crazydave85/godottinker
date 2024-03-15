4a3cc10f72e641b6ed43a470b6fc4a34ced7809b
dave a <crazydave85@gmail.com>
2024-03-15 22:39:45 +0000

    tidying up folders

--- World/world.gd
+++ World/world.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _process(delta):
 	if Input.is_action_just_pressed("Pause"):
-		get_tree().change_scene_to_file("res://menu.tscn")
+		get_tree().change_scene_to_file("res://Menus/menu.tscn")
 	
